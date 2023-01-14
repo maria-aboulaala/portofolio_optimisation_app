6940482126dbefc8846ca3b437cadaec87ccb007
maria-aboulaala <mariaaboulaala28@gmail.com>
2023-01-14 14:06:11 +0100

    first try

---
+++ presentation.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Optimisation d un portfeuille de crypto utilisant le machine learning')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.markdown(
+    """
+---
+ Realisé par 
+ > Aboulaala Maria |  Aberhouch Anass | Mahdioui Med Amine
+
+    """
+)
+
