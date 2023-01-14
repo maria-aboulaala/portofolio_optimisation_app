c7e520d9622910c56731ba5ebe0aa55d82c1eb22
maria-aboulaala <mariaaboulaala28@gmail.com>
2023-01-14 14:12:09 +0100

    c

--- presentation.py
+++ presentation.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 
 st.title('Optimisation d un portfeuille de crypto utilisant le machine learning')
 
 
 
+st.subheader('Entrez les parametres: :key: ')
 
+
+with st.form(key="my_form"):   
+    stock_name = st.selectbox(
+    'Le symbole du stock',
+    ('AAPL', 'MSFT', 'META', 'GOOG', 'AMZN'))    
+    st.form_submit_button("Simuler")
 
 
 
